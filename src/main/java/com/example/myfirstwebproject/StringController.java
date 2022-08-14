b2dc80080cd74d634214d07330cf49a9f903ac5e
= <ekaterina.s.12358@gmail.com>
2022-08-14 22:03:37 +0300

    Добавила возможность добавлять значения в список (addNumber) Добавила проверку есть ли число в списке (isInList)

--- src/main/java/com/example/myfirstwebproject/StringController.java
+++ src/main/java/com/example/myfirstwebproject/StringController.java
@@ -1,31 +1,32 @@
 package com.example.myfirstwebproject;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequiredArgsConstructor // для final полей
-
+@RequiredArgsConstructor
 public class StringController {
+
     private final StringService service;
 
     @GetMapping("/returnString")
     public String returnString() {
         return service.returnString();
     }
+
     @GetMapping("/chooseString")
     @ResponseBody
-    public String chooseString(@RequestParam int number){
+    public String chooseString(@RequestParam int number) {
         return service.chooseString(number);
     }
 
-}
+    @PostMapping("/addNumber")
+    public void addNumber(@RequestParam int number) {
+        service.addNumber(number);
+    }
 
-// в браузере указать какую-то цифру и получить эту цифру в контрллер и вернуть строку с этой цифрой
-// service // component // rest controller // repository
+    @GetMapping("/isInList")
+    public boolean isInList(@RequestParam int number) {
+        return service.isInList(number);
+    }
+}
--- src/main/java/com/example/myfirstwebproject/StringService.java
+++ src/main/java/com/example/myfirstwebproject/StringService.java
@@ -2,15 +2,28 @@ package com.example.myfirstwebproject;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class StringService {
-    public String returnString () {
+
+    List<Integer> numbers = new ArrayList<>();
+
+    public String returnString() {
         return "Hello world!";
     }
 
-    public String chooseString (int number) {
+    public String chooseString(int number) {
         return "Это цифра: " + number;
     }
+
+    public void addNumber(int number) {
+        numbers.add(number);
+    }
+
+    public boolean isInList(int number) {
+        return numbers.contains(number);
+    }
+
 }
-
-
